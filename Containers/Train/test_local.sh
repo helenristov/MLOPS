ee267c20047e84aa29c3d84c696ca2d87f5cca95
Serebellum <helen.ristov@gmail.com>
2023-05-19 15:37:58 -0500

    Add files via upload

---
+++ Containers/Train/test_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# chmod 744 *.sh
+
+PROJECT_ID=$(gcloud config config-helper --format "value(configuration.properties.core.project)")
+BUCKET="${PROJECT_ID}-kubeflowpipelines-default"   # taxi-mlops-demo-kubeflowpipelines-default
+
+# gsutil cp gs://mlops_model_input/data_input/Taxi_Trips_3mo.csv taxi.csv
+
+../build_container.sh
+docker pull gcr.io/${PROJECT_ID}/mlops-demo-train:latest
+docker run -t gcr.io/${PROJECT_ID}/mlops-demo-train:latest ${BUCKET}
